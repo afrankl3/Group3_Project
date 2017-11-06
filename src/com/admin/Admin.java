aa935aadc1c78afaf7d45340f74a174970a55248
afrankl3 <31904060+afrankl3@users.noreply.github.com>
2017-11-06 13:35:11 -0500

    Creating the base classes

--- src/com/admin/Admin.java
+++ src/com/admin/Admin.java
@@ -5,4 +5,12 @@ import com.user.User;
 public class Admin extends User{
     boolean isAdmin = false;
 
+    public boolean areAmin(){
+        if (getUserName().contains("@umw.edu")) {
+            return isAdmin = true;
+        }
+        else {
+            return isAdmin = false;
+        }
+    }
 }
--- src/com/user/User.java
+++ src/com/user/User.java
@@ -2,5 +2,21 @@ package com.user;
 
 public class User {
     String userName;
+    String name;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
