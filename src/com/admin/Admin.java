fa910a853ea3948eb9d460951c0d862673ac725b
afrankl3 <31904060+afrankl3@users.noreply.github.com>
2017-11-06 13:06:03 -0500

    Creating the base classes

---
+++ src/com/admin/Admin.java
@@ -0,0 +1,8 @@
+package com.admin;
+
+import com.user.User;
+
+public class Admin extends User{
+    boolean isAdmin = false;
+    
+}
---
+++ src/com/user/User.java
@@ -0,0 +1,6 @@
+package com.user;
+
+public class User {
+    String userName;
+
+}
