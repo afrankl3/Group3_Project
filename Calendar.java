801fae87b846720b8293ef2ad0de605d722109c5
mkelly11 <31897174+mkelly11@users.noreply.github.com>
2017-11-12 14:15:15 -0500

    Add files via upload
    Heres another option for the calendar, we would need to change it a little though.

--- Calendar.java
+++ Calendar.java
@@ -1,46 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package calendar;
-
-import java.util.Locale;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
 /**
  *
- * @author Keri
+ * @author maddiekelly1
  */
-public class Calendar extends Application {
+public class Calendar {
 
-    private Stage stage;
-    DatePicker checkInDatePicker = new DatePicker();
+ /***************************************************************************
+    *  Given the month, day, and year, return which day
+    *  of the week it falls on according to the Gregorian calendar.
+    *  For month, use 1 for January, 2 for February, and so forth.
+    *  Returns 0 for Sunday, 1 for Monday, and so forth.
+    ***************************************************************************/
+    public static int day(int month, int day, int year) {
+        int y = year - (14 - month) / 12;
+        int x = y + y/4 - y/100 + y/400;
+        int m = month + 12 * ((14 - month) / 12) - 2;
+        int d = (day + x + (31*m)/12) % 7;
+        return d;
+    }
+
+    // return true if the given year is a leap year
+    public static boolean isLeapYear(int year) {
+        if  ((year % 4 == 0) && (year % 100 != 0)) return true;
+        if  (year % 400 == 0) return true;
+        return false;
+    }
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        launch(args);
-    }
+        int month = Integer.parseInt(args[0]);    // month (Jan = 1, Dec = 12)
+        int year = Integer.parseInt(args[1]);     // year
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        this.stage = stage;
-        initUI(stage);
-    }
+        // months[i] = name of month i
+        String[] months = {
+            "",                               // leave empty so that months[1] = "January"
+            "January", "February", "March",
+            "April", "May", "June",
+            "July", "August", "September",
+            "October", "November", "December"
+        };
 
-    private void initUI(Stage stage) {
-        Pane base = new Pane();
-        Scene scene = new Scene(base, 500, 400, Color.WHITESMOKE);
-        stage.setTitle("Reservation Calendar");
-        
-        DatePicker datePicker = new DatePicker();
-        base.getChildren().add(datePicker);
-        
-        
-        stage.setScene(scene);
-        stage.show();
-    }
+        // days[i] = number of days in month i
+        int[] days = {
+            0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+        };
 
+        // check for leap year
+        if (month == 2 && isLeapYear(year)) days[month] = 29;
+
+
+        // print calendar header
+        StdOut.println("   " + months[month] + " " + year);
+        StdOut.println(" S  M Tu  W Th  F  S");
+
+        // starting day
+        int d = day(month, 1, year);
+
+        // print the calendar
+        for (int i = 0; i < d; i++)
+            StdOut.print("   ");
+        for (int i = 1; i <= days[month]; i++) {
+            StdOut.printf("%2d ", i);
+            if (((i + d) % 7 == 0) || (i == days[month])) StdOut.println();
+        }
+
+    }
 }
-
 
