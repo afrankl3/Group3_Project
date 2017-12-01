a67277d1c38eadb49b9319f081e691d069c469e6
mkelly11 <31897174+mkelly11@users.noreply.github.com>
2017-12-01 13:40:34 -0500

    Add files via upload

--- VariousMethods.java
+++ VariousMethods.java
@@ -16,6 +16,36 @@ import java.util.Comparator;
 public class VariousMethods extends Room {
     ArrayList<Room> roomList = new ArrayList<>();
     java.util.Scanner input = new java.util.Scanner(System.in);
+    
+    //Trinkle
+    roomList.add("Trinkle Room1", 20, true, false);
+    roomList.add("Trinkle Room2", 40, true, false);
+    roomList.add("Trinkle Room3", 10, false, false);
+    roomList.add("Trinkle Room4", 100, true, false);
+    roomList.add("Trinkle Room5", 30, false, false);
+    
+    //HCC
+    roomList.add("Convergence Center Room1", 20, true, false);
+    roomList.add("Convergence Center Room2", 40, true, false);
+    roomList.add("Convergence Center Room3", 10, true, false);
+    roomList.add("Convergence Center Room4", 30, true, false);
+    roomList.add("Convergence Center Room5", 100, true, false);
+    
+    //Combs
+    roomList.add("Combs Room1", 40, true, false);
+    roomList.add("Combs Room2", 10, true, false);
+    roomList.add("Combs Room3", 20, false, false);
+    roomList.add("Combs Room4", 50, true,false);
+    roomList.add("Combs Room5", 30, false, false);
+    
+    //Woodard
+    roomList.add("Woodard Room1", 15, false, false); 
+    roomList.add("Woodard Room2", 20, true, false);
+    roomList.add("Woodard Room3", 10, false, false);
+    roomList.add("Woodard Room4", 75, true, false);
+    roomList.add("Woodard Room5", 30, false, false);
+    
+    
     public void printAll(){
         
             for (int i=0; i<roomList.size(); i++){
